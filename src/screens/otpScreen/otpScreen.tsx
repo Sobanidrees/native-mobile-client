616981488624152e5ef771af278eea73c1f87ed7
talhashabbir6954 <talha.shabbir@amperortech.com>
2023-08-01 01:48:28 +0500

    OTP screen added for inspectors verification

--- src/screens/otpScreen/otpScreen.tsx
+++ src/screens/otpScreen/otpScreen.tsx
@@ -10,7 +10,7 @@ import {
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParams } from '../../../routes/routeStack';
 import { PRIMARY_COLOR, RED_ERROR, WHITE } from '../../utils/colors';
-
+import { styles } from './styles';
 type Props = NativeStackScreenProps<RootStackParams, 'OTPInput'>;
 
 const CELL_COUNT = 4;
@@ -82,41 +82,3 @@ export function OTPInput({ navigation }: Props) {
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    root: { flex: 1, padding: 60 },
-    title: { textAlign: 'center', fontSize: 30 },
-    codeFieldRoot: { marginTop: 20 },
-    cell: {
-        width: 40,
-        height: 40,
-        lineHeight: 38,
-        fontSize: 24,
-        borderWidth: 2,
-        borderColor: '#00000030',
-        textAlign: 'center',
-    },
-    focusCell: {
-        borderColor: '#000',
-    },
-    resendOtp: {
-        color: RED_ERROR,
-        fontWeight: '500',
-        fontSize: 15,
-        alignSelf: 'center',
-        marginTop: 20,
-    },
-    verifyBtn: {
-        paddingVertical: '3%',
-        paddingHorizontal: '10%',
-        backgroundColor: PRIMARY_COLOR,
-        borderRadius: 20,
-        marginTop: 20,
-    },
-    btnText: {
-        fontWeight: '600',
-        color: WHITE,
-        fontSize: 20,
-        alignSelf: 'center',
-    },
-});
---
+++ src/screens/otpScreen/styles.tsx
@@ -0,0 +1,44 @@
+import { Dimensions, StyleSheet } from 'react-native';
+import { PRIMARY_COLOR, RED_ERROR, WHITE } from '../../utils/colors';
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export const styles = StyleSheet.create({
+    root: { flex: 1, padding: 60 },
+    title: { textAlign: 'center', fontSize: 30 },
+    codeFieldRoot: { marginTop: 20 },
+    cell: {
+        width: 40,
+        height: 40,
+        lineHeight: 38,
+        fontSize: 24,
+        borderWidth: 2,
+        borderColor: '#00000030',
+        textAlign: 'center',
+    },
+    focusCell: {
+        borderColor: '#000',
+    },
+    resendOtp: {
+        color: RED_ERROR,
+        fontWeight: '500',
+        fontSize: 15,
+        alignSelf: 'center',
+        marginTop: 20,
+    },
+    verifyBtn: {
+        paddingVertical: '3%',
+        paddingHorizontal: '10%',
+        backgroundColor: PRIMARY_COLOR,
+        borderRadius: 20,
+        marginTop: 20,
+    },
+    btnText: {
+        fontWeight: '600',
+        color: WHITE,
+        fontSize: 20,
+        alignSelf: 'center',
+    },
+});
