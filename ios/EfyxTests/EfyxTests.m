72de6a8bb88ac9bff2f7dd9ab1f72b95ee4b03b4
Saqib Ejaz <saqibejaz285@gmail.com>
2023-08-14 18:57:26 -0400

    Changed app name from NativeMobileClient to Efyx

--- ios/EfyxTests/EfyxTests.m
+++ ios/EfyxTests/EfyxTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NativeMobileClientTests : XCTestCase
+@interface EfyxTests : XCTestCase
 
 @end
 
-@implementation NativeMobileClientTests
+@implementation EfyxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
